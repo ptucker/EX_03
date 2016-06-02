89f108e55a888ec4bd742d1aec14bfb0f36dbd82
gbingham18 <gbingham18@my.whitworth.edu>
2016-06-01 22:35:11 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,127 +1,127 @@
 // Grant Bingham
 // CS 172
-// Ex. 9.6
-// UML
-//----------------------
-// Class QuadraticEquation
-//----------------------
-// - int a, b, c
-//----------------------
-// + QuagraticEquation()
-// + QuadraticEquation(int newa, int newb, int newc)
-// + int getA()
-// + int getB()
-// + int getC()
-// + void changeA()
-// + void changeB()
-// + void changeC()
-// + int getDiscriminant()
-// + double getRoot1()
-// + double getRoot2()
+// Ex. 10.10
+
+//UML
+//Class: MyInteger
+//------------------
+// - int value
+//------------------
+// + MyInteger()
+// + MyInteger(int setvalue)
+// + int getValue()
+// + const bool isEven()
+// + const bool isOdd()
+// + const bool isPrime()
+// + static bool isEven(int newvalue)
+// + static bool isOdd(int newvalue)
+// + static bool isPrime(int newvalue)
+// + static bool isEven(const MyInteger& newvalue)
+// + static bool isOdd(const MyInteger& newvalue)
+// + static bool isPrime(const MyInteger& newvalue)
+
 #include <iostream>
 using namespace std;
 
-class QuadraticEquation
+class MyInteger
 {
 	private:
-		int a, b, c;
-
-	public:	
-	
-	QuadraticEquation()
-	{
-		a = 1;
-		b = 1;
-		c = 1;
-	}
-
-
-	QuadraticEquation(int newa, int newb, int newc)
-	{
-		a = newa;
-		b = newb;
-		c = newc;
-	}
-
-	int getA()
-	{
-		return a;
-	}
-	int getB()
-	{
-		return b;
-	}
-	int getC()
-	{
-		return c;
-	}
-
-	void changeA(int seta)
-	{
-		a = seta;
-		return;
-	}
-	void changeB(int setb)
-	{
-		b = setb;
-		return;
-	}
-	void changeC(int setc)
-	{
-		c = setc;
-		return;
-	}
-	int getDiscriminant()
-	{
-		int d;
-		d = (b * b) - (4 * a * c);
-		if (d < 0)
+		int value;
+	public:
+		MyInteger()
 		{
-			return 0;
+			value = 0;
 		}
-		else
+
+		MyInteger(int setvalue)
 		{
-			return d;
+			value = setvalue;
 		}
-	}
-	double getRoot1()
-	{
-		double r1;
-		r1 = (-b + sqrt(getDiscriminant())) / static_cast<double>(2 * a);
-		return r1;
-	}
-	double getRoot2()
-	{
-		double r2;
-		r2 = (-b - sqrt(getDiscriminant())) / static_cast<double>(2 * a);
-		return r2;
-	}
-};
+
+		int getValue()
+		{
+			return value;
+		}
+
+		const bool isEven()
+		{
+			if (value % 2 == 0)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		const bool isOdd()
+		{
+			if (value % 2 == 1)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		const bool isPrime()
+		{
+			for (int i = 1; i < value; i++)
+			{
+				if (value % i == 0)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		static bool isEven(int newvalue)
+		{
+			if (newvalue % 2 == 0)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		static bool isOdd(int newvalue)
+		{
+			if (newvalue % 2 == 1)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		static bool isPrime(int newvalue)
+		{
+			for (int i = 1; i < newvalue; i++)
+			{
+				if (newvalue % i == 0)
+				{
+					return false;
+				}	
+			}
+			return true;
+		}
+
+		static bool isEven(const MyInteger& newvalue)
+		{
+
+		}
+		
+		static bool isOdd(const MyInteger& newvalue)
+		{
+
+		}
+
+		static bool isPrime(const MyInteger& newvalue)
+		{
+
+		}
+
+};			
 
 int main()
 {
-	cout << "Enter Values for a, b, and c:" << endl;
-	int e, f, g;
-	cin >> e >> f >> g;
-	QuadraticEquation Quad1(e,f,g);
-	if (Quad1.getDiscriminant() > 0)
-	{
-		cout << Quad1.getRoot1() << endl;
-		cout << Quad1.getRoot1() << endl;
-	}
-	else if (Quad1.getDiscriminant() == 0)
-	{
-		if (Quad1.getRoot1() >= 0)
-		{
-			cout << Quad1.getRoot1() << endl;
-		}
-		else
-		{
-			cout << Quad1.getRoot2() << endl;
-		}
-	}
-	else
-	{
-		cout << "This equation has no real roots." << endl;
-	}
+	MyInteger val();
+	MyInteger val1();
 }
